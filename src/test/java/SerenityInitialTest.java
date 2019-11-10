a7242ccdacc61be2f7d6e226707b281fba7c1eca
Karen Agualimpia <h2otatiana@gmail.com>
2019-11-10 15:16:42 -0500

    Se agregan ajuste al test, con respecto a la question que se crea, se llama desde el test.

--- src/test/java/SerenityInitialTest.java
+++ src/test/java/SerenityInitialTest.java
@@ -2,11 +2,14 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abiities.CallAnApi;
-import net.serenitybdd.screenplay.rest.interactions.Get;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import questions.ResponseCode;
+import tasks.GetUsers;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SerenityRunner.class)
 public class SerenityInitialTest {
@@ -23,8 +26,14 @@ public class SerenityInitialTest {
             GetUsers.fromPage(2) //deme los usuarios de la panina 2
         );
 
-        //si la peticion es un exito, debe responder 200 (en postman es e status ok)
+        /*si la peticion es un exito, debe responder 200 (en postman es e status ok)
         assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+        0
+       assertThat(ResponseCode.was()).isEqualTo(200);
+       */
+        Karen.should(
+                seeThat("El codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
     }
 
 }
