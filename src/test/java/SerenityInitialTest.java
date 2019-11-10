4c05412ab40d21b9d9f0416b773c2072a41bfb1a
Karen Agualimpia <h2otatiana@gmail.com>
2019-11-10 12:47:46 -0500

    Se agregan dos escenarios, uno exitoso y otro que falle, para ver e comportamiento.

--- src/test/java/SerenityInitialTest.java
+++ src/test/java/SerenityInitialTest.java
@@ -1,2 +1,49 @@
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abiities.CallAnApi;
+import net.serenitybdd.screenplay.rest.interactions.Get;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SerenityRunner.class)
 public class SerenityInitialTest {
+
+    private static final String restApiUrl = "http://localhost:5000/api";
+
+    @Test
+    public void getUsers(){
+        //Creamos el actor que tendra la habilidad de poder comunicarse a traves de la api on la url
+        Actor Karen = Actor.named("Karen the trainer")
+                .whoCan(CallAnApi.at(restApiUrl));
+
+        Karen.attemptsTo(
+                Get.resource("/users?page=2")
+        );
+
+        //si la peticion es un exito, debe responder 200 (en postman es e status ok)
+        assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+    }
+
+    /*
+    Para este esenario responde:
+    Expected :400
+    Actual   :200
+     */
+    @Test
+    public void getUsersFail(){
+        //Creamos el actor que tendra la habilidad de poder comunicarse a traves de la api on la url
+        Actor Karen = Actor.named("Karen the trainer")
+                .whoCan(CallAnApi.at(restApiUrl));
+
+        Karen.attemptsTo(
+                Get.resource("/users?page=2")
+        );
+
+        //si la peticion es un exito, debe responder 200 (en postman es e status ok)
+        assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(400);
+    }
+
 }
