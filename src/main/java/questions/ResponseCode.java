73ae8ab4d6bab18c795478972434727466671939
Karen Agualimpia <h2otatiana@gmail.com>
2019-11-10 15:17:27 -0500

    Se crea questios quien preguntara el estado del request.

---
+++ src/main/java/questions/ResponseCode.java
@@ -0,0 +1,20 @@
+package questions;
+
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResponseCode implements Question {
+
+    public static Question<Integer> was(){
+        return new ResponseCode();
+    }
+
+    @Override
+    public Object answeredBy(Actor actor) {
+         //si la peticion es un exito, debe responder 200 (en postman es e status ok)
+        return SerenityRest.lastResponse().statusCode();
+    }
+}
